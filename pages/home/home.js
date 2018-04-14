be961cd84c127223e1dd5516ea7bb64600311d87
360 <759005093@qq.com>
2018-04-14 13:07:19 +0800

    今日天气

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const utils = require('../../utils/util');
+const QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
 const app = getApp()
 
 Page({
@@ -15,13 +16,16 @@ Page({
     interval: 5000,
     duration: 1000,
     newData: {},
-    loading: true
+    loading: true,
+    weather: {}
   },
   onLoad() {
 
   },
   onReady() {
+    let self = this;
     this.getNews();
+    this.getLocation()
   },
   gotoEatWhat() {
     let url = 'eatWhat';
@@ -31,9 +35,60 @@ Page({
     this.setData({
       loading: true
     })
-    this.getNews(function(){
+    this.getNews(function () {
       wx.stopPullDownRefresh();
     })
+  },
+  getLocation() {
+    let self = this;
+    let getLocation = utils.wxPromisify(wx.getLocation);
+    getLocation({ type: 'wgs84' })
+      .then(res => {
+
+        let qqmapsdk = new QQMapWX({
+          key: 'QLIBZ-4SRKX-VZ54Z-ZYLR4-GJOSO-K7BYQ'
+        });
+
+        let options = {
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: function (res) {
+            let detail = res.result.address_component;
+            let url = "https://wis.qq.com/weather/common";
+            let data = {
+              source: 'xw',
+              weather_type: "forecast_1h",
+              province: detail.province,
+              city: detail.city,
+              county: detail.district
+            }
+
+            utils.get(url,data)
+            .then(res =>{
+              console.log(res.data.data.forecast_1h[1])
+              let weather = res.data.data.forecast_1h[1];
+              weather.city = data.city;
+              weather.county = data.county;
+              weather.image = `https://mat1.gtimg.com/pingjs/ext2020/weather/mobile2.0/assets/weather/day/${weather.weather_code}.png`
+              weather.update_time = utils.formatTime(new Date());
+              self.setData({
+                weather: weather
+              })
+            })
+          }
+        }
+        // 地址逆解析
+        qqmapsdk.reverseGeocoder(options);
+
+      })
+      .catch((x)=>{
+        console.log(x);
+      })
+  },
+  getweather(res) {
+    console.log(res);
   },
   getNews(callback) {
     let url = 'https://m.toutiao.com/list/';
@@ -54,7 +109,7 @@ Page({
         self.setData({
           newData: res.data.data
         })
-        if(callback) {
+        if (callback) {
           callback()
         }
         this.setData({
